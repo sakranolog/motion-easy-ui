bc43c5167d67689d89309a1558704a74d4adfc91
sakranolog <124002683+sakranolog@users.noreply.github.com>
2024-07-12 16:52:27 +0300

    Update server.js to serve Public with capital P

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('Public'));
 app.use(bodyParser.json());
 
 const instance = axios.create({
